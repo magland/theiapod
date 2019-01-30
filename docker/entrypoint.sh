f3cd69af5899d8c27b5cb0de461a53adc5ff9dc2
Jeremy Magland <jeremy.magland@gmail.com>
2019-01-30 21:11:04 +0000

    entrypoint: symbolic links to files in home directory

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,9 +17,20 @@ chown -R $USER:$USER /venv
 #	chown $USER:$USER /theiapod_init
 #fi
 
-cat >/the_script.sh <<EOL
+## IMPORTANT to put single quotes around EOL here so that the stuff does not get interpretted
+cat >/the_script.sh <<'EOL' 
 #!/bin/bash
 set -ex
+
+# symbolic links to files in /home/theiapod to the home directory
+if [ -d /home/theiapod ]; then
+    echo "test"
+    ls /home
+    ls /home/theiapod/.*
+    for filename in $(ls -A /home/theiapod/); do
+        ln -s /home/theiapod/$filename ~/$filename
+    done
+fi
 
 cd $PROJECT_DIRECTORY
 source /venv/bin/activate
@@ -33,4 +44,4 @@ yarn theia start $PROJECT_DIRECTORY --hostname=0.0.0.0 --port=$PORT
 EOL
 chmod a+x /the_script.sh
 
-sudo -u $USER bash -c "/the_script.sh"
+sudo -u $USER bash -c "PROJECT_DIRECTORY=$PROJECT_DIRECTORY PORT=$PORT /the_script.sh"
